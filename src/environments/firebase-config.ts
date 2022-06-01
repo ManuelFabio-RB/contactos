984f6842c2efd6e216caa02341aeebbae579217a
ManuelFabio-RB <manuel.fabio.rb@gmail.com>
2022-06-01 01:37:52 -0500

    creacion e app contactos

---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAZCP-6SK020MkRSOs4zJj70vt-epBC3uk",
+    authDomain: "contactos-85655.firebaseapp.com",
+    projectId: "contactos-85655",
+    storageBucket: "contactos-85655.appspot.com",
+    messagingSenderId: "215534944611",
+    appId: "1:215534944611:web:e9038b8a9796e19a3bd356",
+    measurementId: "G-3HV28DKGRB"
+  };
